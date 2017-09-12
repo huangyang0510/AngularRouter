e19c7010ca12bb0d3e0d772bb6c43c7dc2fd70d3
huangyang0510 <540113773@qq.com>
2017-09-13 01:58:01 +0800

    构建普通路由

---
+++ src/app/hero-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template:`
+      <h2>HEROS</h2>
+      <p>Get your heros here</p>
+      <button routerLink="/sidekicks" >Go to sidekicks</button>
+    `
+})
+
+export class HeroListComponent {
+
+}
